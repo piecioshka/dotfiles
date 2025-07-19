f10ab899efb1b60ee1338533b0c2b2ded4460d12
Piotr Kowalski <piecioshka@gmail.com>
2025-07-19 13:12:13 +0200

    Aliases: change name "search" to "z"

--- configs/shells/__aliases.sh
+++ configs/shells/__aliases.sh
@@ -19,8 +19,7 @@ alias ff='find . -not -path "*/node_modules/*" -not -path "*/.git/*" -iname'
 alias grep='grep --color=auto'
 alias lastfile='find . -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | head -5 | cut -f2- -d" " | xargs ls -ld' # Get file with last modified time.
 alias mode="stat -f '%p %N'"
-alias search="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
-alias search-open="code \$(search)"
+alias z="code \$(fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}')"
 alias rgg="rg --glob=!node_modules"
 alias exif-clean="exiftool -all= -overwrite_original"
 
