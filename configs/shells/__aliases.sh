e9202d7af6ae80c6a496a66eb5660f38791390bb
Piotr Kowalski <piecioshka@gmail.com>
2025-07-19 13:16:06 +0200

    Aliases: Use npx in "s"

--- configs/shells/__aliases.sh
+++ configs/shells/__aliases.sh
@@ -25,7 +25,7 @@ alias exif-clean="exiftool -all= -overwrite_original"
 # ------------------------------------------------------------------------------
 
 ### Node.js global packages
-alias s='browser-sync start --server --no-ui --no-open --directory --serveStatic .'
+alias s='npx browser-sync start --server --no-ui --no-open --directory --serveStatic .'
 alias sw='s --watch' ## for slides
 
 # ------------------------------------------------------------------------------
